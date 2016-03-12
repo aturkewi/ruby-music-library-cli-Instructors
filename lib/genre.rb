ce456707e8880546d4b19062edac96b4a511b449
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-12 14:28:44 -0500

    Add artist and genre class

---
+++ lib/genre.rb
@@ -0,0 +1,27 @@
+class Genre
+
+  attr_accessor :name
+
+  @@all ||= []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.create(name)
+    self.new(name).tap { | a | a.save }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+end
